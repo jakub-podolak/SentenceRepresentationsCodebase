b8828ea57f96060f7ab424f59bc00568a8089fc4
jakub podolak <jjp241@gmail.com>
2024-04-21 22:37:21 +0200

    Fix bug with the same file changed

--- eval.py
+++ eval.py
@@ -3,7 +3,9 @@ import sys
 import argparse
 import torch
 import numpy as np
+from utils.snli_data import check_and_load_or_save
 from utils.word_embeddings import create_dictionary, get_wordvec, get_word_embeddings
+from utils.eval import evaluate_model  
 
 PATH_TO_SENTEVAL = 'SentEval/'
 PATH_TO_DATA = 'SentEval/data/'
@@ -19,30 +21,27 @@ def parse_option():
     parser.add_argument(
         "--path", type=str, default="None", help="path to pretrained model"
     )
+    parser.add_argument(
+        "--transfer", action=argparse.BooleanOptionalAction
+    )
+    parser.add_argument(
+        "--snli", action=argparse.BooleanOptionalAction
+    )
     args = parser.parse_args()
     return args
 
 
-def main():
-    args = parse_option()
-    print(args)
-
-    assert args.path is not None
-
-    with open(args.path, 'rb') as f:
-        model = torch.load(f)
-        sentence_encoder = model.encoder
-
-    # SentEval prepare and batcher
+def evaluate_transfer_tasks(sentence_encoder, word2id, word2vec):
+     # SentEval prepare and batcher
     def prepare(params, samples):
-        _, params.word2id = create_dictionary(samples)
-        params.word_vec = get_wordvec(PATH_TO_VEC, params.word2id)
+        params.word2id = word2id
+        params.word_vec = word2vec
         params.wvec_dim = 300
         params.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") 
         return
 
     def batcher(params, batch):
-        batch = [sent if sent != [] else ['.'] for sent in batch]
+        batch = [sent if len(sent) > 0 else ['.'] for sent in batch]
         embeddings = []
 
         for sent in batch:
@@ -57,15 +56,59 @@ def main():
     params_senteval = {'task_path': PATH_TO_DATA, 'usepytorch': False, 'kfold': 5, 'seed': 1111}
 
     # Set up logger
-    logging.basicConfig(format='%(asctime)s : %(message)s', level=logging.DEBUG)
+    logging.basicConfig(format='%(asctime)s : %(message)s', level=logging.INFO)
 
     se = senteval.engine.SE(params_senteval, batcher, prepare)
 
     # define transfer tasks
-    transfer_tasks = ['MR', 'CR', 'SUBJ', 'MPQA', 'SST', 'TREC', 'SICKRelatedness', 'SICKEntailment', 'MRPC', 'STS14']
+    transfer_tasks = ['MR', 'CR', 'SUBJ', 'MPQA', 'SST2', 'TREC', 'SICKRelatedness', 'SICKEntailment', 'MRPC', 'STS14']
+    # transfer_tasks = ['MR']
 
     results = se.eval(transfer_tasks)
-    print(results)
+    return results
+
+
+def main():
+    args = parse_option()
+    print(args)
+
+    assert args.path is not None
+
+    with open(args.path, 'rb') as f:
+        model = torch.load(f)
+        sentence_encoder = model.encoder
+
+    # Initialize dictionary from SNLI
+    train = check_and_load_or_save('train')
+    valid = check_and_load_or_save('validation')
+    test = check_and_load_or_save('test')
+
+    all_sentences = list(train['sentence1']) + list(train['sentence2']) +\
+                    list(valid['sentence1']) + list(valid['sentence2']) +\
+                    list(test['sentence1']) + list(test['sentence2'])
+
+    _, word2id = create_dictionary(all_sentences)
+    word2vec = get_wordvec(PATH_TO_VEC, word2id)
+
+    if args.transfer:
+        transfer_results = evaluate_transfer_tasks(sentence_encoder, word2id, word2vec)
+    else:
+        transfer_results = None
+
+    if args.snli:
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        dev_acc = evaluate_model(model, valid, word2vec, batch_size=64, device=device)
+        test_acc = evaluate_model(model, test, word2vec, batch_size=64, device=device)
+    else:
+        dev_acc = None
+        test_acc = None
+    print(sentence_encoder)
+    print('transfer results:')
+    print(transfer_results)
+    print('dev acc:')
+    print(dev_acc)
+    print('test_acc:')
+    print(test_acc)
 
 if __name__ == "__main__":
     main()
--- utils/eval.py
+++ utils/eval.py
@@ -1,114 +1,31 @@
-import logging
-import sys
-import argparse
+from tqdm import tqdm
 import torch
+from sklearn.metrics import accuracy_score
 import numpy as np
-from utils.snli_data import check_and_load_or_save
-from utils.word_embeddings import create_dictionary, get_wordvec, get_word_embeddings
-from utils.eval import evaluate_model  
 
-PATH_TO_SENTEVAL = 'SentEval/'
-PATH_TO_DATA = 'SentEval/data/'
-PATH_TO_VEC = 'pretrained/glove.840B.300d.txt'
+from utils.utils import batch_data
+from utils.word_embeddings import get_word_embeddings
 
-# import SentEval
-sys.path.insert(0, PATH_TO_SENTEVAL)
-import senteval
+def evaluate_model(model, data_to_evaluate, word2vec, batch_size=64, device='cpu', tuple_input=True):
+    model.eval()
 
+    sentence1 = list(data_to_evaluate['sentence1'])
+    sentence2 = list(data_to_evaluate['sentence2'])
+    target = list(data_to_evaluate['target'])
 
-def parse_option():
-    parser = argparse.ArgumentParser("Evaluate Sentence Embedding Models")
-    parser.add_argument(
-        "--path", type=str, default="None", help="path to pretrained model"
-    )
-    parser.add_argument(
-        "--transfer", action=argparse.BooleanOptionalAction
-    )
-    parser.add_argument(
-        "--snli", action=argparse.BooleanOptionalAction
-    )
-    args = parser.parse_args()
-    return args
+    predicted_labels = []
 
+    for (s1_batch, s2_batch) in tqdm(zip(batch_data(sentence1, batch_size), batch_data(sentence2, batch_size)), total=(len(sentence1) // batch_size)):
+        if tuple_input:
+            s1_encoded = (torch.Tensor(np.array([get_word_embeddings(word2vec, s1) for s1 in s1_batch])).to(device), [len(s1) for s1 in s1_batch])
+            s2_encoded = (torch.Tensor(np.array([get_word_embeddings(word2vec, s2) for s2 in s2_batch])).to(device), [len(s1) for s1 in s1_batch])
+        else:
+            s1_encoded = torch.Tensor(np.array([get_word_embeddings(word2vec, s1) for s1 in s1_batch])).to(device)
+            s2_encoded = torch.Tensor(np.array([get_word_embeddings(word2vec, s2) for s2 in s2_batch])).to(device)
+        output = model.forward(s1_encoded, s2_encoded)
+        predicted_classes = torch.argmax(output, dim=-1).detach().cpu().numpy()
+        predicted_labels += list(predicted_classes)
 
-def evaluate_transfer_tasks(sentence_encoder, word2id, word2vec):
-     # SentEval prepare and batcher
-    def prepare(params, samples):
-        params.word2id = word2id
-        params.word_vec = word2vec
-        params.wvec_dim = 300
-        params.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") 
-        return
-
-    def batcher(params, batch):
-        batch = [sent if len(sent) > 0 else ['.'] for sent in batch]
-        embeddings = []
-
-        for sent in batch:
-            word_embeddings = torch.Tensor(np.array([get_word_embeddings(params.word_vec, sent)])).to(params.device)
-
-            sentvec = sentence_encoder.forward((word_embeddings, [len(sent)]))[0].detach().cpu().numpy()
-            embeddings.append(sentvec)
-
-        embeddings = np.vstack(embeddings)
-        return embeddings
-
-    params_senteval = {'task_path': PATH_TO_DATA, 'usepytorch': False, 'kfold': 5, 'seed': 1111}
-
-    # Set up logger
-    logging.basicConfig(format='%(asctime)s : %(message)s', level=logging.INFO)
-
-    se = senteval.engine.SE(params_senteval, batcher, prepare)
-
-    # define transfer tasks
-    transfer_tasks = ['MR', 'CR', 'SUBJ', 'MPQA', 'SST2', 'TREC', 'SICKRelatedness', 'SICKEntailment', 'MRPC', 'STS14']
-    # transfer_tasks = ['MR']
-
-    results = se.eval(transfer_tasks)
-    return results
-
-
-def main():
-    args = parse_option()
-    print(args)
-
-    assert args.path is not None
-
-    with open(args.path, 'rb') as f:
-        model = torch.load(f)
-        sentence_encoder = model.encoder
-
-    # Initialize dictionary from SNLI
-    train = check_and_load_or_save('train')
-    valid = check_and_load_or_save('validation')
-    test = check_and_load_or_save('test')
-
-    all_sentences = list(train['sentence1']) + list(train['sentence2']) +\
-                    list(valid['sentence1']) + list(valid['sentence2']) +\
-                    list(test['sentence1']) + list(test['sentence2'])
-        
-    _, word2id = create_dictionary(all_sentences)
-    word2vec = get_wordvec(PATH_TO_VEC, word2id)
-
-    if args.transfer:
-        transfer_results = evaluate_transfer_tasks(sentence_encoder, word2id, word2vec)
-    else:
-        transfer_results = None
-
-    if args.snli:
-        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        dev_acc = evaluate_model(model, valid, word2vec, batch_size=64, device=device)
-        test_acc = evaluate_model(model, test, word2vec, batch_size=64, device=device)
-    else:
-        dev_acc = None
-        test_acc = None
-    print(sentence_encoder)
-    print('transfer results:')
-    print(transfer_results)
-    print('dev acc:')
-    print(dev_acc)
-    print('test_acc:')
-    print(test_acc)
-
-if __name__ == "__main__":
-    main()
+    return {
+        'accuracy': accuracy_score(target, predicted_labels)
+    }
